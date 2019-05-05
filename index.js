6b82aade118752768df97d59ad6751fef9d68738
spider2346 <33738633+spider2346@users.noreply.github.com>
2019-05-05 13:59:57 -0400

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 48038))
 
 const REQUIRE_AUTH = true
 const AUTH_TOKEN = 'an-example-token'
